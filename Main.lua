bdc282c34e349ffa69eb3ee179315fb9812b7c0d
TheTombstoneBackdoorOwner <mythtester500400@outlook.com>
2025-07-29 17:58:02 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,8 +1,16 @@
 local Players = game:GetService("Players")
 local Chat = game:GetService("Chat")
+local StarterGui = game:GetService("StarterGui")
 
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local head = character:WaitForChild("Head")
 
 Chat:Chat(head, _G.Txt, Enum.ChatColor.Blue)
+
+StarterGui:SetCore("ChatMakeSystemMessage", {
+    Text = player.Name .. ": " .. _G.Txt;
+    Color = Color3.fromRGB(0, 162, 255);
+    Font = Enum.Font.SourceSansBold;
+    FontSize = Enum.FontSize.Size18;
+})
